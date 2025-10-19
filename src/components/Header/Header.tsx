86a1d9cd157355236e1b2a53377efa7c2d52dd98
Yanay Zabary <yanay.tsabary@gmail.com>
2025-10-19 09:45:27 +0300

    Add banner

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ import logoRwhite from "@/assets/logo-r-white.webp";
 function Header() {
   const { theme } = useTheme();
   return (
-    <header className="fixed top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-6 py-4">
+    <header className="fixed top-6 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-6 py-4">
       <div className="flex items-center gap-6 mx-auto max-w-6xl">
         <a href="https://replyke.com" className="flex gap-2 items-end">
           <img
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import Footer from "./Footer";
+import OpenSourceBanner from "./OpenSourceBanner";
 
 const Layout = () => {
   return (
     <div className="relative flex flex-col min-h-screen font-body bg-background overflow-x-hidden">
+      <OpenSourceBanner />
       <Header />
 
-      <main className="flex-1 mt-24">{/* mt-20 to account for fixed header */}
+      <main className="flex-1 pt-24">
+        {/* mt-20 to account for fixed header */}
         <Outlet />
       </main>
       <Footer />
