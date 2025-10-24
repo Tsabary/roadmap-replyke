be807c09fe09da8aeabf0929fcb7563c2aeb59ce
Yanay Zabary <yanay.tsabary@gmail.com>
2025-10-24 11:17:30 +0300

    ppush

--- src/components/AuthDialog/GitHubAuthButton.tsx
+++ src/components/AuthDialog/GitHubAuthButton.tsx
@@ -1,13 +1,25 @@
+import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
 
 function GitHubAuthButton() {
+    const { theme } = useTheme();
+
   const BASE_URL = import.meta.env.VITE_PUBLIC_SERVER_URL;
   const url = BASE_URL + "/auth/github";
+
+  const isDarkTheme =
+    theme === "system"
+      ? typeof window !== "undefined" &&
+        window.matchMedia?.("(prefers-color-scheme: dark)").matches
+      : theme === "dark";
 
   return (
     <Button variant="outline" type="button" className="w-full" asChild>
       <a href={url}>
-        <img src="/github.svg" className="mr-2 h-4 w-4" />
+        <img
+          src={isDarkTheme ? "/github-white.svg" : "/github.svg"}
+          className="mr-2 h-4 w-4"
+        />
         GitHub
       </a>
     </Button>
