4f4e81e73d9579bb69a38f3151cff2c6808c8bb5
Yanay Zabary <yanay.tsabary@gmail.com>
2025-10-24 11:13:12 +0300

    internal

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_PUBLIC_SERVER_URL + "/api";
+const BASE_URL = import.meta.env.VITE_PUBLIC_SERVER_URL + "/internal";
 
 export default axios.create({
   baseURL: BASE_URL,
